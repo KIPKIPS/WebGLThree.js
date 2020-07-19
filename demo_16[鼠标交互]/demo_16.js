f999ae0261d862f495cf8362ac0da7f6f7df92bd
KIPKIPS <2432719656@qq.com>
2020-07-19 17:46:12 +0800

    three.raycaster类的使用

--- demo_16[鼠标交互]/demo_16.js
+++ demo_16[鼠标交互]/demo_16.js
@@ -1,15 +1,14 @@
-    /// <reference path="../js/three.js" />
+/// <reference path="../js/three.js" />
+import * as THREE from '../js/three.module.js';
 var container;
 var scene, raycaster, camera, renderer;
 var mouse = new THREE.Vector2(),
     INTERSECTED;
-var radius = 100,
-    theta = 0;
-
+var radius = 100,theta = 0;
 function draw() {
     container = document.createElement("div");
     document.body.appendChild(container);
-
+    
     var info = document.createElement("div");
     info.style.position = "absolute";
     info.style.top = "10px";
@@ -36,23 +35,23 @@ function draw() {
         obj.rotation.y = Math.random() * 2 * Math.PI;
         obj.rotation.z = Math.random() * 2 * Math.PI;
 
-        obj.scale.x=Math.random()+0.5;
-        obj.scale.y=Math.random()+0.5;
-        obj.scale.z=Math.random()+0.5;
+        obj.scale.x = Math.random() + 0.5;
+        obj.scale.y = Math.random() + 0.5;
+        obj.scale.z = Math.random() + 0.5;
         scene.add(obj);
     }
 
-    raycaster=new THREE.Raycaster();
+    raycaster = new THREE.Raycaster();
 
-    renderer=new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(0xf0f0f0);
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth,window.innerHeight);
-    renderer.sortObjects=false;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.sortObjects = false;
     container.appendChild(renderer.domElement);
 
-    document.addEventListener("mousemove",onDocumentMouseMove,false);
-    window.addEventListener("resize",onWindowResize,false);
+    document.addEventListener("mousemove", onDocumentMouseMove, false);
+    window.addEventListener("resize", onWindowResize, false);
     animate();
 }
 
@@ -60,41 +59,41 @@ function animate() {
     requestAnimationFrame(animate);
     render();
 }
-function onWindowResize(){
-    camera.aspect=window.innerWidth/window.innerHeight;
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
-    renderer.setSize(window.innerWidth,window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
-function onDocumentMouseMove(event){
+function onDocumentMouseMove(event) {
     event.preventDefault();
-    mouse.x=(event.clientX/window.innerWidth)*2-1;
-    mouse.y=-(event.clientY/window.innerHeight)*2+1;
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
 function render() {
-    theta+=0.1;
+    theta += 0.1;
     // renderer.render(scene, camera);
-    camera.position.x=radius*Math.sin(THREE.Math.degToRad(theta));
-    camera.position.y=radius*Math.sin(THREE.Math.degToRad(theta));
-    camera.position.z=radius*Math.cos(THREE.Math.degToRad(theta));
+    camera.position.x = radius * Math.sin(THREE.Math.degToRad(theta));
+    camera.position.y = radius * Math.sin(THREE.Math.degToRad(theta));
+    camera.position.z = radius * Math.cos(THREE.Math.degToRad(theta));
     camera.lookAt(scene.position)
 
     camera.updateMatrixWorld();
 
-    raycaster.setFromCamera(mouse,camera);
-    var intersects=raycaster.intersectObjects(scene.children);
-    if (intersects.length>0) {
-        if (INTERSECTED!=intersects[0].object) {
-            if (INTERSECTED)INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
-            INTERSECTED=intersects[0].object;
+    raycaster.setFromCamera(mouse, camera);
+    var intersects = raycaster.intersectObjects(scene.children);
+    if (intersects.length > 0) {
+        if (INTERSECTED != intersects[0].object) {
+            if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
+            INTERSECTED = intersects[0].object;
             INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
             INTERSECTED.material.emissive.setHex(0xff0000);
         }
     }
-    else{
+    else {
         if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
-        INTERSECTED=null;
+        INTERSECTED = null;
     }
-    renderer.render(scene,camera)
+    renderer.render(scene, camera)
 }
