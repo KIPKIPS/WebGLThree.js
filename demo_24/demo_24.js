f95cea05ddb6079d972c803f6018b6444fc8c4c9
KIPKIPS <2432719656@qq.com>
2020-08-22 23:11:24 +0800

    Update demo_24.js

--- demo_24/demo_24.js
+++ demo_24/demo_24.js
@@ -36,46 +36,7 @@ function init() {
     //组对象
     group = new THREE.Group();
     group.position.y = 50;
-    scene.add(group);
-
-    //贴图
-    // var texture=THREE.ImageUtils.loadTexture("../images/joystick.png");
-    // texture.wrapS=texture.wrapT=THREE.RepeatWrapping;
-    // texture.repeat.set(0.008,0.008);
-
-
-    //创建一个自定义的形状
-    //THREE.Line本质是一个mesh
-    function addShape(shape, color, x, y,z, rx, ry, rz, s) {
-        points = shape.createPointsGeometry(50);
-        points.vertices.push(new THREE.Vector3(180, 0, 0))//把闭合点手动传入,使之成为闭合图形
-        //参数列表 geometry 材质 模式 
-        line = new THREE.Line(points, new THREE.LineBasicMaterial({
-            color: color
-        }))
-        
-        line.position.set(x, y, z - 25);
-        line.rotation.set(rx, ry, rz);
-        line.scale.set(s, s, s);
-        group.add(line);
-    }
-
-    //circle
-    var circleRadius = 180;
-    circleShape = new THREE.Shape();
-    circleShape.moveTo(circleRadius, 0);//移动点到这个坐标
-    circleShape.quadraticCurveTo(0, circleRadius, -circleRadius, 0);//贝塞尔曲线(4个参数为起始点xy和终点xy坐标)
-    //circleShape.vertices.push(new THREE.Vector3(180, 0,0))
-    //参数列表 shape 颜色 图形位置,x,y,z 旋转 x,y,z 缩放
-    addShape(circleShape, 0x00f000, 0, 0, 0, 0, 0, 0, 1);
-
-    renderer=new THREE.WebGLRenderer({antialias:true});
-    renderer.setClearColor(0xf0f0f0);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth,window.innerHeight);
-    container.appendChild(renderer.domElement);
-
-    
+    scene.add(group);  
 }
 animate();
 function animate() {
